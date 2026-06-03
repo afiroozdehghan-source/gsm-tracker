025a61413cc2dfaa20ba42831601c749fcd75766
afiroozdehghan-source <afiroozdehghan@gsmsystems.com>
2026-06-03 21:37:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 # --- CONFIGURATION ---
 # 🛑 حتماً لینک جدید دریافتی از مرحله دیپلوی بالا را اینجا جایگزین کنید
-WEBAPP_URL = "https://script.google.com/macros/s/AKfycbx4haypZoOa0dntbAs9fE-eFreKtx_wvFK25tbegK34IYmj1ZHvavnXYNAsygmmqvuv/exec" 
+WEBAPP_URL = "https://script.google.com/macros/s/AKfycbztx5e-bktU8GV9DnWqpAvj0Q4pmCCuZiEAZmEclxXo1GAEqOnArpJe43oNvB94Ey6j/exec" 
 
 USER_CREDENTIALS = {
     "alireza": "admin2026",
